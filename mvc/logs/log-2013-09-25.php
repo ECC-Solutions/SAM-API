5fdea96971a67c3783103f31028c65bd76dc0a8c
ahmedsaber111 <ahmedsaber111@gmail.com>
2013-09-25 09:21:23 +0200

    ignore files

---
+++ mvc/logs/log-2013-09-25.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 25-09-2013 02:27:56 --> 404 Page Not Found --> favicon.ico
+ERROR - 25-09-2013 02:27:58 --> 404 Page Not Found --> favicon.ico
